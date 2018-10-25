670314471bada0fe275f834e24715cb848a47605
Cristian Diaz Prado <cdp.officesystem.com>
2018-10-25 18:34:41 +0200

    c

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BodyComponent implements OnInit {
 
+
+    code = `
+        function myFunction() {
+        document.getElementById("demo1").innerHTML = "Hello there!";
+        document.getElementById("demo2").innerHTML = "How are you?";
+        }
+    `
+
   constructor() { }
 
   ngOnInit() {
